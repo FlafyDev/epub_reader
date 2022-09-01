ddbdbb37dae05cac092954ca08214cdba1871380
FlafyDev <flafyarazi@gmail.com>
2022-09-01 15:57:00 +0300

    nix: flake use devshell

--- flake.nix
+++ flake.nix
@@ -1,23 +1,55 @@
 {
-  description = "A basic flake with a shell";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    android = {
+      url = "github:tadfisher/android-nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = [ pkgs.bashInteractive ];
-        shellHook = ''
-          export PATH="$PATH:${pkgs.flutter}/bin/cache/dart-sdk/bin"
-        '';
-        buildInputs = with pkgs; [
-          flutter
-          jdk11
-        ];
+  outputs = { self, nixpkgs, devshell, flake-utils, android }:
+    {
+      overlay = final: prev: {
+        inherit (self.packages.${final.system}) android-sdk android-studio;
       };
-    });
+    }
+    //
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.android_sdk.accept_license = true;
+          overlays = [
+            devshell.overlay
+            self.overlay
+          ];
+        };
+      in
+      {
+        packages = {
+          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+            build-tools-29-0-2
+            cmdline-tools-latest
+            platform-tools
+            platforms-android-32
+            emulator
+          ]);
+
+          android-studio = pkgs.androidStudioPackages.stable;
+        };
+
+        devShell = import ./devshell.nix { inherit pkgs; };
+      }
+    );
 }
