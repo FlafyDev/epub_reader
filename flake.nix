e98914adfe0ba9980a0c34f0ffefeacb5a566722
FlafyDev <flafyarazi@gmail.com>
2022-10-25 20:43:49 +0300

    nix: switch to dart-flutter-nix

--- flake.nix
+++ flake.nix
@@ -1,55 +1,32 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs";
-    };
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    android = {
-      url = "github:tadfisher/android-nixpkgs";
+    dart-flutter = {
+      url = "github:flafydev/dart-flutter-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, devshell, flake-utils, android }:
-    {
-      overlay = final: prev: {
-        inherit (self.packages.${final.system}) android-sdk android-studio;
-      };
-    }
-    //
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          config.android_sdk.accept_license = true;
-          overlays = [
-            devshell.overlay
-            self.overlay
-          ];
-        };
-      in
-      {
-        packages = {
-          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
-            build-tools-29-0-2
-            cmdline-tools-latest
-            platform-tools
-            platforms-android-32
-            emulator
-          ]);
-
-          android-studio = pkgs.androidStudioPackages.stable;
-        };
-
-        devShell = import ./devshell.nix { inherit pkgs; };
-      }
-    );
+  outputs = { self, nixpkgs, flake-utils, dart-flutter }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ dart-flutter.overlays.default ];
+          };
+        in
+        {
+          devShell = pkgs.mkFlutterShell {
+            android = {
+              enable = true;
+              buildToolsVersions = [ "29-0-2" ];
+              platformsAndroidVersions = [ "32" ];
+            };
+          };
+        }) // { };
 }
